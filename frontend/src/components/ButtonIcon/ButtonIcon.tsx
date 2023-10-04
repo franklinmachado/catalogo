d501354bafd61e35eaf99618d20dc2f200f2c618
Franklin Machado <frankmcontato@gmail.com>
2023-10-04 00:06:25 -0300

    backed

--- frontend/src/components/ButtonIcon/ButtonIcon.tsx
+++ frontend/src/components/ButtonIcon/ButtonIcon.tsx
@@ -1,12 +1,17 @@
+import { ReactComponent as Seta } from 'assets/images/Seta.svg';
 import './styles.css';
 
 const ButtonIcon = () => {
   return (
-    <>
+    <div className="btn-container">
       <button className="btn btn-primary btn-icon">
         <h6>INICIE AGORA A SUA BUSCA</h6>
       </button>
-    </>
+
+      <div className="btn-icon-container">
+        <Seta />
+      </div>
+    </div>
   );
 };
 
